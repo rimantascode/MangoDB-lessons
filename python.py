8dae87853aafe003b48b3d0bd68823539f7195ad
rimantascode <r.preiks@gmail.com>
2020-07-06 11:22:53 +0000

    mongo db CRUD

--- python.py
+++ python.py
@@ -18,10 +18,16 @@ conn = mongo_connect(MONGODB_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
 
-coll.update_one({'nationality':'american'}, {'$set':{'hair_color':'ginger'} })
+"""coll.update_one({'nationality':'american'}, {'$set':{'hair_color':'ginger'} })"""
+
+coll.insert({'first':'John', 'last':'lennon', 'dob':'09/08/1985', 'gender':'m', 'hair_color':'brown', 'nationality': 'English', 'occupation':'Beatle'})
+
+"""coll.remove({'first':'John'})"""
 
 
-documents = coll.find({'nationality':'american'})
-
+documents = coll.find({"first":"John"})
+i=0
 for doc in documents:
+    i=i+1
     print(doc)
+print("This is how many is inthe document{}".format(i))
