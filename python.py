928516c584d962148935c96b629faeb29cddfb01
rimantascode <r.preiks@gmail.com>
2020-07-06 11:27:35 +0000

    the template of the CRUD

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ import pymongo
 import os
 
 MONGODB_URI = os.getenv("MONGO_URI")
-DBS_NAME = "myFirstMDB"
+DBS_NAME = "mytestdb"
 COLLECTION_NAME = "myFirstMDB"
 
 def mongo_connect(url):
@@ -17,17 +17,9 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
+documents = coll.find()
 
-"""coll.update_one({'nationality':'american'}, {'$set':{'hair_color':'ginger'} })"""
-
-coll.insert({'first':'John', 'last':'lennon', 'dob':'09/08/1985', 'gender':'m', 'hair_color':'brown', 'nationality': 'English', 'occupation':'Beatle'})
-
-"""coll.remove({'first':'John'})"""
-
-
-documents = coll.find({"first":"John"})
-i=0
 for doc in documents:
-    i=i+1
     print(doc)
-print("This is how many is inthe document{}".format(i))
+
+    
